1b873547aeb8c9a775afdcb3cb350e10059c5dc3
FANG <1157880517@qq.com>
2019-05-30 14:56:04 +0800

    3333333

--- js/index.js
+++ js/index.js
@@ -1,10 +1,18 @@
 $(document).ready(function () {
     $('.container').width($(window).width())
     $('.container').height($(window).height())
-
-    setTimeout(function () {
-        $('.container').css('transform','rotateY(-90deg)')
-     
-    }, 5000)
+    console.log($(window).height())
+    $('.resume-name-wrap').click(function(){
+        $('.container').css('transform','rotateX(90deg)')
+        $('.user-heard-wrap').hide(2000)
+        $('.resume-name-wrap').hide(2000)
+        
+    })
+    $('.back').click(function(){
+        $('.container').css('transform','rotateX(0deg)')
+        $('.user-heard-wrap').show(2000)
+        $('.resume-name-wrap').show(2000)
+    })
+   
 })
 
